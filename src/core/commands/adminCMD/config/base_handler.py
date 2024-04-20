31c98240fa7ec0a0314c5f80cb2208161c534c1f
xRedCrystalx <xredcrystaledx@gmail.com>
2024-04-20 14:59:36 +0200

    Modal interaction handler, bug fixes

--- src/core/commands/adminCMD/config/base_handler.py
+++ src/core/commands/adminCMD/config/base_handler.py
@@ -1,71 +1,34 @@
-import sys, typing, discord, traceback
+import sys, typing, discord
 sys.dont_write_bytecode = True
 import src.connector as con
 
-class AdvancedPaginator(discord.ui.View):
-    def __init__(self, current_position: str, timeout: float | None = None) -> None:
+from src.core.helpers.views import ViewHelper
+from src.core.helpers.modals import ModalHelper
+
+class BaseConfigCMDView(ViewHelper):
+    def __init__(self, current_position: str, guild_id: int, timeout: float | None = None) -> None:
         super().__init__(timeout=timeout)
         self.shared: con.Shared = con.shared
-        self._handle_imports()
+        self._handle_imports(guild_id)
 
-        self.global_config: dict[str, dict[str, str | list]] = {
-            "general": {
-                "name": "General settings",
-                "help": self.help_obj.general,
-                "config": self.config_obj.general
-            },
-            "cmd": {
-                "name": "Commands",
-                "help": self.help_obj.commands,
-                "config": self.config_obj.commands
-            },
-            "alt": {
-                "name": "Alt Detection",
-                "help": self.help_obj.altDetection,
-                "config": self.config_obj.altDetection
-            },
-            "imper": {
-                "name": "Impersonator Detection",
-                "help": self.help_obj.impersonatorDetection,
-                "config": self.config_obj.imperDetection
-            },
-            "ai": {
-                "name": "Artificial Intelligence",
-                "help": self.help_obj.ai,
-                "config": self.config_obj.ai
-            },
-            "automod": {
-                "name": "Automod Response",
-                "help": self.help_obj.automod,
-                "config": self.config_obj.automod
-            },
-            "link": {
-                "name": "Link Protection",
-                "help": self.help_obj.linkProtection,
-                "config": self.config_obj.linkProtection
-            },
-            "ping": {
-                "name": "Ping Protection",
-                "help": self.help_obj.pingProtection,
-                "config": self.config_obj.pingProtection
-            },
-            "auto_delete": {
-                "name": "Auto Delete",
-                "help": self.help_obj.autoDelete,
-                "config": self.config_obj.autoDelete
-            },
-            "auto_slowmode": {
-                "name": "Auto Slowmode",
-                "help": self.help_obj.autoSlowmode,
-                "config": self.config_obj.autoSlowmode
-            }
+        self.global_config: dict[str, str] = {
+            "general": "General settings",
+            "cmd": "Commands",
+            "alt": "Alt Detection",
+            "imper": "Impersonator Detection",
+            "ai": "Artificial Intelligence",
+            "automod": "Automod Response",
+            "link":"Link Protection",
+            "ping": "Ping Protection",
+            "auto_delete": "Auto Delete",
+            "auto_slowmode": "Auto Slowmode",
         }
         
         self.page_names: list[str] = list(self.global_config.keys())
         self.page_len: int = len(self.page_names)-1
         self.current_position: str = current_position if current_position else self.page_names[-1]
 
-    def _handle_imports(self) -> None:
+    def _handle_imports(self, guild_id: int) -> None:
         self.shared.logger.log(f"@AdvancedPaginator._handle_imports > Importing handlers.", "NP_DEBUG")
         from .pages import HelpPages, ConfigPages
         from .configurator import Configurator
@@ -73,7 +36,7 @@ class AdvancedPaginator(discord.ui.View):
         try:
             self.help_obj: HelpPages = HelpPages()
             self.config_obj: ConfigPages = ConfigPages()
-            self.configurator: Configurator = Configurator()
+            self.configurator: Configurator = Configurator(guild_id)
             self.shared.logger.log(f"@AdvancedPaginator._handle_imports > Imported and executed handlers..", "NP_DEBUG")
 
         except Exception as error:
@@ -82,11 +45,8 @@ class AdvancedPaginator(discord.ui.View):
     def create_back_button(self, item: bool = False) -> typing.Self | discord.Button:
         button = discord.ui.Button(label="↩", custom_id="PAGINATOR:RETURN", style=discord.ButtonStyle.red)
         if item:
-            self.shared.logger.log(f"@AdvancedPaginator.create_back_button > Created BACK button.", "NP_DEBUG")
             return button
-        else:
-            self.shared.logger.log(f"@AdvancedPaginator.create_back_button > Created and appended BACK button to view.", "NP_DEBUG")
-            return self.add_item(button)
+        return self.add_item(button)
     
     def create_paginator_buttons(self) -> typing.Self:
         self.add_item(discord.ui.Button(label="◄", custom_id="PAGINATOR:BACK", style=discord.ButtonStyle.blurple))
@@ -99,32 +59,46 @@ class AdvancedPaginator(discord.ui.View):
 
     async def update_message(self, interaction: discord.Interaction, data: dict[str, typing.Any]) -> None:
         self.shared.logger.log(f"@AdvancedPaginator.update_message > Updating message...", "NP_DEBUG")
+        if interaction.response.is_done():
+            await interaction.followup.send(**data)
+
         await interaction.response.edit_message(**data)
 
-    async def change_page(self, custom_id: str, interaction: discord.Interaction) -> None:
-        guild_db: dict = self.shared.db.load_data(interaction.guild.id)
+    async def create_modal(self, title: str, custom_id: str, text_inputs: list[discord.TextInput]) -> dict[str, str | list[dict]] | None:
+        modal: ModalHelper = ModalHelper(title=title, custom_id=custom_id)
+        for item in text_inputs:
+            modal.add_item(item)
+
+        await self.interaction.response.send_modal(modal)
+        data: dict = await modal.clean_data()
+        
+        if data.get("error") or not data:
+            return
+        return data
+
+    async def change_page(self, custom_id: str) -> None:
+        guild_db: dict = self.shared.db.load_data(self.interaction.guild.id)
         
         if custom_id.endswith(":BACK") or custom_id.endswith(":NEXT"):
             value: int = 1 if custom_id.endswith(":NEXT") else -1
-
             currentPage: int = self.page_names.index(self.current_position)
+
             index: int = 0 if currentPage + value > self.page_len else self.page_len if currentPage + value < 0 else currentPage + value
             self.current_position = self.page_names[index]
 
             if config := guild_db.get(self.current_position):
                 self.shared.logger.log(f"@AdvancedPaginator.change_page > Changing page to {self.current_position}.", "NP_DEBUG")
-                cnf: dict[str, str | discord.Embed] = self.global_config.get(self.current_position)
-                await self.update_message(interaction, {"embed": self.config_obj.create_embed(config, name=cnf["name"], interaction=interaction, blueprint_embed=cnf["config"])})
+                await self.update_message(self.interaction, {"embed": self.config_obj.create_embed(config, name=self.global_config.get(self.current_position), interaction=self.interaction, blueprint_embed=getattr(self.config_obj, self.current_position, None))})
             else:
-                raise ValueError(f"Could not find data for {self.current_position} in {interaction.guild.name} ({interaction.guild.id}) >> Triggered by PAGINATOR interaction.")
+                raise ValueError(f"Could not find data for {self.current_position} in {self.interaction.guild.name} ({self.interaction.guild.id}) >> Triggered by PAGINATOR interaction.")
 
         elif custom_id.endswith(":INFO"):
             self.shared.logger.log(f"@AdvancedPaginator.change_page > Changing page to INFO.", "NP_DEBUG")
-            await self.update_message(interaction, {"embed": self.global_config.get(self.current_position)["help"], "view": self.clear_items().create_back_button()})
+            await self.update_message(self.interaction, {"embed": getattr(self.help_obj, self.current_position, None), "view": self.clear_items().create_back_button()})
 
         elif custom_id.endswith(":STOP"):
             self.shared.logger.log(f"@AdvancedPaginator.change_page > Changing page to STOP.", "NP_DEBUG")
-            await self.update_message(interaction, {"view": self.clear_items()})
+            await self.update_message(self.interaction, {"view": self.clear_items()})
             self.stop()
 
         elif custom_id.endswith(":RETURN"):
@@ -132,32 +106,35 @@ class AdvancedPaginator(discord.ui.View):
             self.configurator.reset_configurator()
             if config := guild_db.get(self.current_position):
                 self.shared.logger.log(f"@AdvancedPaginator.change_page > Changing page to RETURN.", "NP_DEBUG")
-                cnf: dict[str, str | discord.Embed] = self.global_config.get(self.current_position)
-                await self.update_message(interaction, {"embed": self.config_obj.create_embed(config, name=cnf["name"], interaction=interaction, blueprint_embed=cnf["config"]), "view": self.clear_items().create_paginator_buttons()})
+                await self.update_message(self.interaction, {"embed": self.config_obj.create_embed(config, name=self.global_config.get(self.current_position), interaction=self.interaction, blueprint_embed=getattr(self.config_obj, self.current_position, None)), "view": self.clear_items().create_paginator_buttons()})
             else:
-                raise ValueError(f"Could not find data for {self.current_position} in {interaction.guild.name} ({interaction.guild.id}) >> Triggered by RETURN interaction.")
+                raise ValueError(f"Could not find data for {self.current_position} in {self.interaction.guild.name} ({self.interaction.guild.id}) >> Triggered by RETURN interaction.")
 
     async def interaction_check(self, interaction: discord.Interaction) -> bool:
         if not interaction.data:
             raise ValueError("Discord did not provide interaction data. >> Triggered by INTERACTION_CHECK")
 
         custom_id: str = dict(interaction.data ).get('custom_id')
+        self.interaction: discord.Interaction = interaction
         self.shared.logger.log(f"@AdvancedPaginator.interaction_check > Recieved interaction with ID: {custom_id}.", "NP_DEBUG")
 
         if interaction.type == discord.InteractionType.component:
             if custom_id.startswith("PAGINATOR:"):
-                await self.change_page(custom_id, interaction)
+                await self.change_page(custom_id)
             elif custom_id.startswith("CONFIG:"):
-                await self.configurator.handle_interaction(custom_id, interaction, self)
+                await self.configurator.handle_interaction(custom_id, self)
             else:
                 raise NameError("Could not resolve custom_id of the interaction. >> Triggered by INTERACTION_CHECK")
         return
     
     async def on_error(self, interaction: discord.Interaction, error: Exception, item: discord.ui.item.Item) -> None:
         error_id: str = self.shared._create_id()
-        self.shared.logger.log(f"@AdvancedPaginator.on_error > Error ID: {error_id}\n{traceback.format_exc()}", "ERROR")
+        self.shared.logger.log(f"BaseConfigCMDView.on_error[{error_id}] >\n{self.shared.errors.full_traceback()}")
 
         error_embed: discord.Embed = self.help_obj.ERROR
         error_embed.description = error_embed.description.format(error=error_id)
 
         await self.update_message(interaction, {"embed": error_embed, "view": self.clear_items()})
+
+async def setup(bot) -> None:
+    pass
