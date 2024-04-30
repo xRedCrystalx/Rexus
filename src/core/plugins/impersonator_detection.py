f85ee153971a713f1d9473b611f17fe09ee25a46
xRedCrystalx <xredcrystaledx@gmail.com>
2024-04-30 14:49:09 +0200

    Ready for V3

--- src/core/plugins/impersonator_detection.py
+++ src/core/plugins/impersonator_detection.py
@@ -2,6 +2,7 @@ import discord, sys, typing
 sys.dont_write_bytecode = True
 import src.connector as con
 
+
 class ImpersonatorDetection:
     def __init__(self) -> None:
         self.shared: con.Shared = con.shared
@@ -11,37 +12,33 @@ class ImpersonatorDetection:
         embed.set_thumbnail(url=member.display_avatar.url)
         return embed
 
-    async def detection_on_join(self, guild_db: dict[str, typing.Any], bot_db: dict[str, typing.Any], member: discord.Member, **OVERFLOW) -> dict[typing.Any, dict[str, typing.Any]] | None:
+    async def detection_on_join(self, guild_db: dict[str, typing.Any], bot_db: dict[str, typing.Any], member: discord.Member, **OVERFLOW) -> None:
         if member and guild_db["imper"]["status"] and (channel_id := guild_db["imper"]["log_channel"]):
             names: list[str] = bot_db["filters"]["impersonator"]["names"]
-            if member.display_name.lower() in names or str(member.global_name).lower() in names and member.id not in names:
-                try:
-                    embed: discord.Embed = self._create_embed(member)
-                    embed.add_field(name="`` Member ``", value=f"<:profile:1203409921719140432>┇{member.display_name}\n<:global:1203410626492240023>┇{member.global_name}\n<:ID:1203410054016139335>┇{member.id}", inline=True)
-                    embed.add_field(name="`` Rule ``", value=f"**Member joined with Celebrity/YouTuber/Bot/Staff name.**", inline=True)
-                    embed.add_field(name="`` Kick Message ``", value=f"```Impersonating a youtuber/celebrity/bot/staff. Come back when you've changed your name or you can request a nickname from staff.```", inline=False)
+            whitelisted: list[int] = bot_db["filters"]["impersonator"]["whitelist"]
 
-                    if channel := member.guild.get_channel(channel_id):
-                        return [{channel: {"action": "send", "kwargs": {"embed": embed}}}]
+            if member.display_name.lower() in names or str(member.global_name).lower() in names and member.id not in whitelisted:
+                embed: discord.Embed = self._create_embed(member)
+                embed.add_field(name="`` Member ``", value=f"<:profile:1203409921719140432>┇{member.display_name}\n<:global:1203410626492240023>┇{member.global_name}\n<:ID:1203410054016139335>┇{member.id}", inline=True)
+                embed.add_field(name="`` Rule ``", value=f"**Member joined with Celebrity/YouTuber/Bot/Staff name.**", inline=True)
+                embed.add_field(name="`` Kick Message ``", value=f"```Impersonating a youtuber/celebrity/bot/staff. Come back when you've changed your name or you can request a nickname from staff.```", inline=False)
 
-                except Exception as error:
-                    self.shared.logger.log(f"@ImpersonatorDetection.detection_on_join: {type(error).__name__}: {error}", "ERROR")
+                if channel := member.guild.get_channel(channel_id):
+                    self.shared.sender.resolver(con.Event(channel, "send", event_data={"embed": embed}))
         return None
 
-    async def detection_on_update(self, guild_db: dict[str, typing.Any], bot_db: dict[str, typing.Any], before: discord.Member, after: discord.Member, **OVERFLOW) -> dict[typing.Any, dict[str, typing.Any]] | None:
-        if after.global_name != before.global_name or before.display_name != after.display_name:
+    async def detection_on_update(self, guild_db: dict[str, typing.Any], bot_db: dict[str, typing.Any], before: discord.Member, after: discord.Member, **OVERFLOW) -> None:
+        if after.global_name != before.global_name or after.display_name != before.display_name:
             if guild_db["imper"]["status"] and (channel_id := guild_db["imper"]["log_channel"]):
                 names: list[str] = bot_db["filters"]["impersonator"]["names"]
-                if after.display_name.lower() in names or str(after.global_name).lower() in names and after.id not in names:
-                    try:
-                        embed: discord.Embed = self._create_embed(after)
-                        embed.add_field(name="`` Before ``", value=f"<:profile:1203409921719140432>┇{before.display_name}\n<:global:1203410626492240023>┇{before.global_name}\n<:ID:1203410054016139335>┇{before.id}", inline=True)
-                        embed.add_field(name="`` After ``", value=f"<:profile:1203409921719140432>┇{after.display_name}\n<:global:1203410626492240023>┇{after.global_name}\n<:ID:1203410054016139335>┇{after.id}", inline=True)
-                        embed.add_field(name="`` Reason ``", value=f"Member changed name to Celebrity/YouTuber/Bot/Staff name. name.\n\n```Impersonating a youtuber/celebrity/bot/staff. Come back when you've changed your name or you can request a nickname from staff.```", inline=False)
+                whitelisted: list[int] = bot_db["filters"]["impersonator"]["whitelist"]
 
-                        if channel := after.guild.get_channel(channel_id):
-                            return [{channel: {"action": "send", "kwargs": {"embed": embed}}}]
+                if after.display_name.lower() in names or str(after.global_name).lower() in names and after.id not in whitelisted:
+                    embed: discord.Embed = self._create_embed(after)
+                    embed.add_field(name="`` Before ``", value=f"<:profile:1203409921719140432>┇{before.display_name}\n<:global:1203410626492240023>┇{before.global_name}\n<:ID:1203410054016139335>┇{before.id}", inline=True)
+                    embed.add_field(name="`` After ``", value=f"<:profile:1203409921719140432>┇{after.display_name}\n<:global:1203410626492240023>┇{after.global_name}\n<:ID:1203410054016139335>┇{after.id}", inline=True)
+                    embed.add_field(name="`` Reason ``", value=f"Member changed name to Celebrity/YouTuber/Bot/Staff name. name.\n\n```Impersonating a youtuber/celebrity/bot/staff. Come back when you've changed your name or you can request a nickname from staff.```", inline=False)
 
-                    except Exception as error:
-                        self.shared.logger.log(f"@ImpersonatorDetection.detection_on_update: {type(error).__name__}: {error}", "ERROR")
+                    if channel := after.guild.get_channel(channel_id):
+                        self.shared.sender.resolver(con.Event(channel, "send", event_data={"embed": embed}))
         return None
