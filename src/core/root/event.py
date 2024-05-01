9e07f6764f7fb9ca5cb5ce16d3345e31960ff46a
xRedCrystalx <xredcrystaledx@gmail.com>
2024-05-01 20:04:59 +0200

    Removed useless method

--- src/core/root/event.py
+++ src/core/root/event.py
@@ -1,14 +1,9 @@
 import sys, typing, dataclasses
 sys.dont_write_bytecode = True
 
-
 @dataclasses.dataclass
 class Event:
     event_obj: typing.Callable
     action: str
     event_data: dict[str, typing.Any]
     _async: bool = True
-
-    @classmethod
-    def create_event(cls, event_obj: typing.Callable, action: str, event_data: dict[str, typing.Any], _async: bool = True) -> "Event":
-        return Event(event_obj=event_obj, action=action, event_data=event_data, _async=_async)
