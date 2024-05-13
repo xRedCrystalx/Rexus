c09ed144f0ce76936aedcb1a5d6202cda9893219
xRedCrystalx <xredcrystaledx@gmail.com>
2024-05-13 22:07:32 +0200

    Better start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-cd $HOME/noping
+cd $HOME/NoPing
+echo "Updating from git.."
 git pull noping v3
 
 if [ ! -d ".venv" ]; then
+    echo ".venv folder does not exist in the current directory, creating a new one."
     python3 -m venv .venv
+else
+    echo ".venv folder exists in the current directory. Ignoring."
 fi
-source .venv/bin/activate
+echo "Activating virtual env and updating requirements."
 
+source .venv/bin/activate
 pip install -r requirements.txt
 
 clear
