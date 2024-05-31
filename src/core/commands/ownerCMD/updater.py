44b39ee34331b5bb7b25e967f4834cb5b334c427
xRedCrystalx <xredcrystaledx@gmail.com>
2024-05-31 19:24:48 +0200

    Added full system update

--- src/core/commands/ownerCMD/updater.py
+++ src/core/commands/ownerCMD/updater.py
@@ -12,7 +12,8 @@ class Updater(commands.Cog):
 
     @app_commands.choices(cmd=[
         app_commands.Choice(name="fetch_from_github", value="fetch"),
-        app_commands.Choice(name="reload", value="reload")
+        app_commands.Choice(name="reload", value="reload"),
+        app_commands.Choice(name="full_reload", value="full")
         ]
     )
     @app_commands.command(name="updater", description="Owner commands, no touchy!")
@@ -23,7 +24,7 @@ class Updater(commands.Cog):
 
         if interaction.user.id in bot_config.get("owners", []):
             if cmd.value == "fetch":
-                process: subprocess.CompletedProcess[bytes] = subprocess.run(["git", "pull", "noping", "v3"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, text=False)
+                subprocess.run(["git", "pull", "noping", "v3"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, text=False)
                 await interaction.followup.send(content="Fetched latest version from github.")
             elif cmd.value == "reload":
                 if bot_config["reloader"].get(args):
@@ -32,6 +33,9 @@ class Updater(commands.Cog):
                 else:
                     self.shared.reloader.reload_discord_module(args)
                     await interaction.followup.send(content=f"Reloaded discord module on path: {args}")
+            elif cmd.value == "full":
+                for module in bot_config["reloader"]:
+                    self.shared.reloader.reload_module(module)
         else:
             await interaction.followup.send("You do not have permissions to execute this command.", ephemeral=True)
 
