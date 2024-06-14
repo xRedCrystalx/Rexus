f35e73e1aa1bef31ff334c14380efa7cb5bd356b
xRedCrystalx <xredcrystaledx@gmail.com>
2024-06-14 23:16:30 +0200

    Complete re-desing

--- src/core/plugins/ping_protection.py
+++ src/core/plugins/ping_protection.py
@@ -1,54 +1,54 @@
 import discord, sys, typing
 sys.dont_write_bytecode = True
 import src.connector as con
-from xRedUtils.dates import get_datetime
+
+from src.core.helpers.permissions import check_ids
+from src.core.helpers.embeds import create_base_embed, apply_embed_items
+from src.core.helpers.event import Event
 
 class PingProtection:
     def __init__(self) -> None:
         self.shared: con.Shared = con.shared
 
     async def find_pings(self, guild_db: dict[str, typing.Any], message: discord.Message = None, after: discord.Message = None,  **OVERFLOW) -> None:
+        # check for on_message or on_message_edit events - one is always `None`
         message: discord.Message = message or after
         
-        if guild_db["ping"]["status"] and message.channel.id not in guild_db["ping"]["ignoredChannels"] and not message.author.bot:
-            pingedMembers: list[discord.Member | discord.User] = message.mentions if guild_db["ping"]["detectReplyPings"] else [message.guild.get_member(user_id) for user_id in message.raw_mentions]
-            user_role_ids: list[int] = [role.id for role in message.author.roles]
-            staff_role: int = guild_db["general"]["staffRole"]
+        # plugin disabled, bot, ignored channel
+        if not guild_db["ping"]["status"] and check_ids([message.channel], guild_db["ping"]["ignoredChannels"]) and message.author.bot:
+            return
+            
+        pingedMembers: list[discord.Member | discord.User] = message.mentions if guild_db["ping"]["detectReplyPings"] else [message.guild.get_member(user_id) for user_id in message.raw_mentions]
+        staff_role: int = guild_db["general"]["staffRole"]
 
-            #checks if pinged users have any of protection roles
-            if pingedMembers:
-                for user in pingedMembers:
-                    role_IDs: list = [role.id for role in user.roles]
-                    events: list[con.Event] = []
-                    
-                    # now we check every custom rule:
-                    for rule_name, rule_data in guild_db["ping"]["rules"].items():
-                        # has bypass or not protected, skipping
-                        if rule_data.get("role") not in role_IDs or rule_data.get("bypass") in user_role_ids:
-                            continue
+        #checks if pinged users have any of protection roles
+        for member in pingedMembers:
+            
+            #checks every custom rule:
+            for rule_name, rule_data in guild_db["ping"]["rules"].items():
+                # has bypass or not protected, skipping
+                if not check_ids(rule_data.get("role"), member.roles) or check_ids([rule_data.get("bypass")], member.roles):
+                    continue
+                
+                # whole notification logic - once executed, check will stop
+                embed = create_base_embed("**Hold up!**", description=f"{message.author.mention} Please do not ping this user. If you need help or have questions, please ping <@&{staff_role}> instead!\n\n**As always, read the server rules!**")
+                self.shared.sender.resolver(Event(message.channel, "send", event_data={"kwargs": {"embed": embed, "delete_after": 30}})) 
 
-                        embed = discord.Embed(title="**Hold up!**", 
-                                              description=f"{message.author.mention} Please do not ping this user. If you need help or have questions, please ping <@&{staff_role}> instead!\n\n**As always, read the server rules!**", 
-                                              color=discord.Colour.dark_embed(), timestamp=get_datetime())
-                        events.append(con.Event(message.channel, "send", event_data={"kwargs": {"embed": embed, "delete_after": 30}}))
+                if rule_data.get("ping"):
+                    self.shared.sender.resolver(Event(message.channel, "send", event_data={"kwargs": {"content": f"Calling staff team: <@&{staff_role}>"}}))
 
-                        if rule_data.get("ping"):
-                            events.append(con.Event(message.channel, "send", event_data={"kwargs": {"content": f"Calling staff team: <@&{staff_role}>"}}))
+                if rule_data.get("delete"):
+                    self.shared.sender.resolver(Event(message, "delete", event_data={}))
 
-                        if rule_data.get("delete"):
-                            events.append(con.Event(message, "delete", event_data={}))
+                # log message
+                if rule_data.get("log") and (log_channel_id := rule_data.get("logChannel")):
+                    embed: discord.Embed =  apply_embed_items(
+                        embed=create_base_embed(f"Ping Protection - {rule_name}"),
+                        thumbnail=message.author.display_avatar.url)
+                    embed.add_field(name="`` Author ``", value=f"<:profile:1203409921719140432>┇{message.author.display_name}\n<:global:1203410626492240023>┇{message.author.global_name}\n<:ID:1203410054016139335>┇{message.author.id}", inline=True)
+                    embed.add_field(name="`` Location ``", value=f"<:msg_id:1203422168046768129>┇{message.id}\n<:text_c:1203423388320669716>┇{message.channel.name}\n<:ID:1203410054016139335>┇{message.channel.id}", inline=True)
+                    embed.add_field(name="`` Message ``", value="Could not find the message content." if len(message.content) == 0 else message.content if len(message.content) < 1000 else f"{message.content[:1000]}...", inline=False)
 
-                        if rule_data.get("log") and (log_channel_id := rule_data.get("logChannel")):
-                            if log_channel := message.guild.get_channel(log_channel_id):
-                                embed: discord.Embed = discord.Embed(title=f"Ping Protection - {rule_name}", timestamp=get_datetime(), color=discord.Colour.dark_embed())
-                                embed.set_thumbnail(url=message.author.display_avatar.url)
-                                embed.add_field(name="`` Author ``", value=f"<:profile:1203409921719140432>┇{message.author.display_name}\n<:global:1203410626492240023>┇{message.author.global_name}\n<:ID:1203410054016139335>┇{message.author.id}", inline=True)
-                                embed.add_field(name="`` Location ``", value=f"<:msg_id:1203422168046768129>┇{message.id}\n<:text_c:1203423388320669716>┇{message.channel.name}\n<:ID:1203410054016139335>┇{message.channel.id}", inline=True)
-                                if (msglen := len(message.content)) > 1000:
-                                    embed.add_field(name="`` Message ``", value=f"{message.content[:msglen-1000]}...", inline=False)
-                                else:
-                                    embed.add_field(name="`` Message ``", value=message.content if msglen != 0 else "Could not find the message content.", inline=False)
-    
-                                events.append(con.Event(log_channel, "send", event_data={"kwargs": {"embed": embed}}))
-
-                    self.shared.sender.resolver(events)
+                    if log_channel := message.guild.get_channel(log_channel_id):
+                        self.shared.sender.resolver(Event(log_channel, "send", event_data={"kwargs": {"embed": embed}}))
+                return None
