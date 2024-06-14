d7ede6447deb2797a64a65e2307a9fff29d638dd
xRedCrystalx <xredcrystaledx@gmail.com>
2024-06-14 23:17:52 +0200

    New desing, work in progress

--- src/core/plugins/hacked_acc_detection.py
+++ src/core/plugins/hacked_acc_detection.py
@@ -3,7 +3,8 @@ sys.dont_write_bytecode = True
 from discord.ext import commands
 import src.connector as con
 
-from xRedUtils.dates import get_datetime
+from src.core.helpers.embeds import create_base_embed, apply_embed_items
+from src.core.helpers.event import Event
 
 class HackedAccounts:
     def __init__(self) -> None:
@@ -13,49 +14,30 @@ class HackedAccounts:
             "porn": ["18+", "teen", "girls", "free", "leaks", "onlyfans", "nude", "hot", "pussy", "porn", "nsfw", "sex", "video", "photo", "content", ":underage:", ":peach:", ":heart:", ":sweat_drops:", ":egg_plant:", ":hot_face:", ":lips:"],
             #"acc": ["gift card", "money", "[steamcommunity.com/gift"]
         }
-        self.bad_guilds: dict[int, int] = {}
-
-    async def kick(self, member: discord.Member, kick: bool) -> None:
-        if not member: return
-
-        if kick:
-            self.shared.sender.resolver(con.Event(member, "kick", event_data={"kwargs": {"reason": "Hacked account"}}))
-
-        embed: discord.Embed = discord.Embed(title="Hacked Accounts Detection", timestamp=get_datetime(), color=discord.Colour.dark_embed())
-        embed.add_field(name="`` Member ``", value=f"<:profile:1203409921719140432>┇{member.display_name}\n<:global:1203410626492240023>┇{member.global_name}\n<:ID:1203410054016139335>┇{member.id}", inline=True)
-        embed.add_field(name="`` Rule ``", value=f"Detected patterns of hacked account behaviour.")
-        embed.set_thumbnail(url=member.display_avatar.url)
-        embed.set_footer(text="Member has been kicked from the server." if member else "Suspicious member. Please check their messages.")
-
-        self.shared.sender.resolver(con.Event(self.bot.get_channel(711311257570902109), "send", event_data={"kwargs": {"embed": embed}}))
 
     async def check_hacked(self, guild_db: dict[str, typing.Any], bot_db: dict[str, typing.Any], action: discord.Message = None, message: discord.Message = None, **OVERFLOW) -> None:
         action = action or message
+        
         # temp. replacing with permissions when making it public
         if action.guild.id == 626159471386689546:
-            if (invites := await self.shared.fetch_invite_links(action.content, option="scam_guilds")):
-                member: discord.Member | None = getattr(action, "member", None) or getattr(action, "author", None)
-                self.shared.logger.log(f"@HackedAccounts.check_hacked: Got {member.name} ({member.id}) -> {invites}", "TESTING")
-                for guild_id in invites:
-                    if guild_id in bot_db["filters"]["hacked_accs_detection"]["bad_guilds"]:
-                        return await self.kick(member, kick=True)
-                    else:
-                        if not self.bad_guilds.get(guild_id):
-                            self.bad_guilds[guild_id] = 0
+            member: discord.Member | None = getattr(action, "member", None) or getattr(action, "author", None)
 
-                        self.bad_guilds[guild_id] += 1
+            for check in self.hacked_types:
+                matches: list[str] = len([item for item in self.hacked_types[check] if item in action.content])
+                
+                if not matches:
+                    return
+                
+                elif matches > 4:
+                    embed: discord.Embed = apply_embed_items(
+                        embed=create_base_embed("Hacked Accounts Protection"),
+                        thumbnail=member.display_avatar.url,
+                        footer="Member has been kicked from the server.")
+                    embed.add_field(name="`` Member ``", value=f"<:profile:1203409921719140432>┇{member.display_name}\n<:global:1203410626492240023>┇{member.global_name}\n<:ID:1203410054016139335>┇{member.id}", inline=True)
+                    embed.add_field(name="`` Rule ``", value=f"Detected patterns of hacked account behaviour.")
 
-                        if self.bad_guilds[guild_id] >= 5:
-                            self.bad_guilds.pop(guild_id)
-                            bot_db["filters"]["hacked_accs_detection"]["bad_guilds"].append(guild_id)
-                            self.shared.db.save_data(None, bot_db)
-
-                for check in self.hacked_types:
-                    matches: list[str] = [item for item in self.hacked_types[check] if item in action.content]
-                    
-                    if len(matches) > 2 and len(matches) <= 4:
-                        await self.kick(member, kick=False) 
-                    elif len(matches) > 4:
-                        return await self.kick(member, kick=True)
-
-# NOTE: Possible memory leak for self.bad_guilds
+                    self.shared.sender.resolver([
+                        Event(member, "kick", event_data={"kwargs": {"reason": "Hacked account"}}),
+                        Event(self.bot.get_channel(711311257570902109), "send", event_data={"kwargs": {"embed": embed}})
+                    ])
+                    return
