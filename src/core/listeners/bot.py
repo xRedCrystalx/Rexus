d315b4c69c94e6fa51ee564bce21e3e0aea1a47d
xRedCrystalx <xredcrystaledx@gmail.com>
2024-06-14 23:07:51 +0200

    Safer DB creation - fixed errors

--- src/core/listeners/bot.py
+++ src/core/listeners/bot.py
@@ -3,7 +3,9 @@ sys.dont_write_bytecode = True
 from discord.ext import commands
 import src.connector as con
 
-from xRedUtils.dates import get_datetime
+from src.core.helpers.embeds import create_base_embed, apply_embed_items
+from src.core.helpers.errors import report_error
+from src.core.helpers.event import Event
 
 class BotListeners(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
@@ -16,32 +18,30 @@ class BotListeners(commands.Cog):
         await self.bot.tree.sync(guild = discord.Object(id=guild.id))
         channel: discord.TextChannel = guild.system_channel
 
-        # read attempt, db handler will create or read database, if nothing returned, error happened. -- TODO: creation of db here
+        # read attempt, db handler will create or read database, if nothing returned, error happened.
         database: dict[str, typing.Any] = self.shared.db.load_data(guild.id)
 
         if not database:
-            id: str = self.shared._create_id()
-            await channel.send(f"An error has occured. Please report this to the developer. Error ID: `{id}`")
-            self.shared.logger.log(f"Failed to create database file for guild {guild.name} ({guild.id}). Error ID: {id}\n{self.shared.errors.full_traceback()}", "ERROR")
+            error_id: str = report_error(None, self.on_guild_join, "full")
+            await channel.send(f"An error has occured. Please report this to the developer. Error ID: `{error_id}`")
 
-        embed: discord.Embed = discord.Embed(title="Hey there!", timestamp=get_datetime(), color=discord.Colour.dark_embed(),
-                                             description=f"Firstly, thank you for inviting me to your guild!\n\nI'm **NoPing**, friendly little robot that will help you protect your community.\nTo start with my configuration, use </config:1235708858580860929> command or </noping:1235708858136002623> for quick start pointers.")
-        embed.set_thumbnail(url="https://i.ibb.co/R6WZm04/member.png")
-        embed.set_footer(text="Developed by xRedCrystalx")
-        await channel.send(embed=embed)
+        embed: discord.Embed = apply_embed_items(
+            embed=create_base_embed("Hey there!", title="Firstly, thank you for inviting me to your guild!\n\nI'm **NoPing**, friendly little robot that will help you protect your community.\nTo start with my configuration, use </config:1235708858580860929> command or </noping:1235708858136002623> for quick start pointers."),
+            thumbnail="https://i.ibb.co/R6WZm04/member.png",
+            footer="Developed by xRedCrystalx"
+        )
+        self.shared.sender.resolver(Event(channel, "send", event_data={"kwargs": {"embed": embed}}))
 
         if events_channel := self.bot.get_channel(self.events_channel_id):
-            embed = discord.Embed(title=f"Joined {guild.name}", description=f"**Guild:** {guild.name} (`{guild.id}`)\n**Creation date:** {guild.created_at:%d.%m.%Y %H:%M:%S}\n**Owner:** {guild.owner.display_name}, {guild.owner.global_name} (`{guild.owner_id}`)",
-                                   timestamp=get_datetime(), color=discord.Colour.green())
+            embed = create_base_embed(f"Joined {guild.name}", description=f"**Guild:** {guild.name} (`{guild.id}`)\n**Creation date:** {guild.created_at:%d.%m.%Y %H:%M:%S}\n**Owner:** {guild.owner.display_name}, {guild.owner.global_name} (`{guild.owner_id}`)", color=discord.Colour.green())
             embed.add_field(name="`` Counts ``", value=f"**Members:** `{guild.member_count}`\n**Roles:** `{len(guild.roles)}`\n**Channels:** `{len(guild.channels)}`\n**Emojis:** `{len(guild.emojis)}`")
-            await events_channel.send(embed=embed)
+            self.shared.sender.resolver(Event(events_channel, "send", event_data={"kwargs": {"embed": embed}}))
 
     @commands.Cog.listener()
     async def on_guild_remove(self, guild: discord.Guild) -> None:
         if events_channel := self.bot.get_channel(self.events_channel_id):
-            embed = discord.Embed(title=f"Left {guild.name}", description=f"**Guild:** {guild.name} (`{guild.id}`)\n**Creation date:** {guild.created_at:%d.%m.%Y %H:%M:%S}\n**Owner:** {guild.owner.display_name}, {guild.owner.global_name} (`{guild.owner_id}`)",
-                                   timestamp=get_datetime(), color=discord.Colour.red())
-            await events_channel.send(embed=embed)
+            embed: discord.Embed = create_base_embed(f"Left {guild.name}", description=f"**Guild:** {guild.name} (`{guild.id}`)\n**Creation date:** {guild.created_at:%d.%m.%Y %H:%M:%S}\n**Owner:** {guild.owner.display_name}, {guild.owner.global_name} (`{guild.owner_id}`)", color=discord.Colour.red())
+            self.shared.sender.resolver(Event(events_channel, "send", event_data={"kwargs": {"embed": embed}}))
 
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(BotListeners(bot))
