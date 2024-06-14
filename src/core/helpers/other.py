6a16ea1c6a4ab3288889a02e9c0beb92fe002311
xRedCrystalx <xredcrystaledx@gmail.com>
2024-06-14 23:28:37 +0200

    Fixed 'not iterable' error for empty link fetch attempt

--- src/core/helpers/other.py
+++ src/core/helpers/other.py
@@ -5,7 +5,7 @@ import src.connector as con
 async def fetch_invite_links(string: str, option: typing.Literal["simon", "scam_guilds"] = None) -> list[int]:
     invites: list[int] = []
     for link in con.shared.global_db["invite_links"].get("regex").findall(string):
-        if link not in con.shared.global_db["invite_links"].get(option):
+        if link not in con.shared.global_db["invite_links"].get(option, []):
             try:
                 invite_link: discord.Invite = await con.shared.bot.fetch_invite(link)
                 invites.append(invite_link.guild.id)
