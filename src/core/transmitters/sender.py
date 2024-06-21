8e69e964573786d0babfea42193b19cd5da9364a
xRedCrystalx <xredcrystaledx@gmail.com>
2024-06-21 18:48:42 +0200

    Implemented event returns & optimizations/simplification

--- src/core/transmitters/sender.py
+++ src/core/transmitters/sender.py
@@ -5,6 +5,8 @@ import src.connector as con
 from src.core.helpers.event import Event
 from src.core.helpers.errors import report_error, full_traceback
 
+from xRedUtils.type_hints import ITERABLE, SIMPLE_ANY
+
 class Sender:
     def __init__(self) -> None:
         self.shared: con.Shared = con.shared
@@ -31,55 +33,47 @@ class Sender:
         self.shared.logger.log(f"@Sender.countdown > Removed {id} from ratelimit.", "NP_DEBUG")
 
     async def rate_limit_handler(self, error: discord.HTTPException, event: typing.Callable) -> None:
-        self.shared.logger.log(f"@Sender.rate_limit_handler > Checking for ratelimit exception..", "NP_DEBUG")
         try:
-            json_response: dict[str, typing.Any] = await error.response.json()
-            response_header: dict[str, typing.Any] = dict(error.response.headers)
-            self.shared.logger.log(f"@Sender.rate_limit_handler > Retrieved response headers and json data.", "NP_DEBUG")
-        
-            if error.response.status == 429:
-                if json_response.get("global") and (retry_after := json_response.get("retry_after") or response_header.get("Retry-After")):
+            try:
+                json_error: dict[str, SIMPLE_ANY] = await error.response.json()
+            except:
+                json_error: dict[str, SIMPLE_ANY] = dict(error.response.headers)
+
+            if error.status == 429:
+                if json_error.get("global") and (retry_after := (json_error.get("retry_after") or json_error.get("Retry-After"))):
                     self.shared.logger.log(f"@Sender.rate_limit_handler > Detected global rate limit. Waiting {retry_after} seconds.", "SYSTEM")
                     # this should block `start` task for x seconds - global ratelimit ?? if safe
                     return await asyncio.sleep(retry_after)
-                
-                elif retry_after := (json_response.get("retry_after") or response_header.get("Retry-After")):
+
+                elif retry_after := (json_error.get("retry_after") or json_error.get("Retry-After")):
                     self.rate_limited.update(ID := getattr(event, "id"), retry_after)
+                    self.shared.loop.create_task(self.countdown(ID), name=f"ratelimit_countdown_{ID}")
+
                     self.shared.logger.log(f"@Sender.rate_limit_handler > Detected endpoint specific rate limit. Waiting {retry_after} seconds on that endpoint.", "SYSTEM")
-                    return self.shared.loop.create_task(self.countdown(ID), name=f"ratelimit_countdown_{ID}")
-            else:
-                self.shared.logger.log(f"@Sender.rate_limit_handler > Not a rate limit error, returning.", "NP_DEBUG")
 
         except Exception as error:
-            report_error(error, self.rate_limit_handler, "simple")
+            report_error(error, self.rate_limit_handler, "full")
 
     async def start(self) -> None:
         while True:
             while self.events:
                 event: Event = self.events[0]
-
                 try:
-                    event_data: dict = event.event_data
+                    if getattr(event.event_obj, "id", None) not in self.rate_limited:
+                        if function := getattr(event.event_obj, event.action, None):
+                            args: ITERABLE = event.event_data.get("args", ())
+                            kwargs: dict[str, SIMPLE_ANY] = event.event_data.get("kwargs", {})
 
-                    if getattr(event.event_obj, "id", None) in self.rate_limited:
+                            res: SIMPLE_ANY = await function(*args, **kwargs) if event._async else function(*args, **kwargs)
+                            if res and event._ret_id:
+                                self.shared.global_db["returned_events"][event._ret_id] = res
+                    else:
                         self.shared.logger.log(f"@Sender.start > Requested event endpoint under rate limit, adding back to queue.", "NP_DEBUG")
                         self.events.append(event)
-                    else:
-                        self.shared.logger.log(f"@Sender.start > Executing API event.", "NP_DEBUG")
-                        if function := getattr(event.event_obj, event.action, None):
-                            if event._async:
-                                res = await function(*event_data.get("args", ()), **event_data.get("kwargs", {}))
-                            else:
-                                res = function(*event_data.get("args", ()), **event_data.get("kwargs", {}))
 
-                            if res and event._ret_id:
-                                # TODO: logic to append response to global_db after successful execution
-                                pass
-                
                 except Exception as error:
                     if isinstance(error, discord.HTTPException):
                         if await self.rate_limit_handler(error, event.event_obj):
-                            self.shared.logger.log(f"@Sender.start > Under rate limit. Adding back to queue.", "NP_DEBUG")
                             self.events.append(event)
                         else:
                             self.shared.logger.log(f"@Sender.execution.discord.HTTPException[Event]: {"await" if event._async else ""} {event.event_obj.__class__.__name__}.{event.action}() {event.event_data}\n{full_traceback()}", "ERROR")
