1fe664d285810a4109804cf0d286b4dfd6bad299
xRedCrystalx <xredcrystaledx@gmail.com>
2024-07-06 19:22:39 +0200

    Fixes, better stability

--- src/core/helpers/modals.py
+++ src/core/helpers/modals.py
@@ -1,39 +1,39 @@
 import discord, sys
 sys.dont_write_bytecode = True
-from discord.ui import Modal
-import src.connector as con
+from .errors import report_error
 
-
-class ModalHelper(Modal):
+class ModalHelper(discord.ui.Modal):
     def __init__(self, title: str, custom_id: str, timeout: float | None = None) -> None:
-        """Handler for `discord.Modal`
+        """
+        Handler for `discord.Modal`
         
         - my_modal.get_data()     [`dict` | `None`] -> get original dict of returned data or error
         or
-        - my_modal.clean_data()   [`dict` | `None`] -> get filtered/clean dict of returned data or error"""
+        - my_modal.clean_data()   [`dict` | `None`] -> get filtered/clean dict of returned data or error
+        """
         
         super().__init__(title=title, timeout=timeout, custom_id=custom_id)
         
-        self.shared: con.Shared = con.shared
         self.data: None | dict = None
         self.interaction: discord.Interaction = None
     
     async def on_submit(self, interaction: discord.Interaction) -> None:
         self.data = dict(interaction.data) | {"interaction": interaction}
-        await interaction.response.defer()
 
     async def on_error(self, interaction: discord.Interaction, error: Exception) -> None:
-        self.shared.logger.log(f"@ModalHelper.on_error[{interaction.data['custom_id']}] > {type(error).__name__}: {error}", "ERROR")
+        await report_error(f"ModalHelper.on_error<{interaction.data["custom_id"]}>", "full")
         self.data = {"error": error, "interaction": interaction}
-    
+
     async def on_timeout(self) -> None:
+        self.data = {"error": "Timed out."}
         self.stop()
 
     async def get_data(self) -> dict[str, str | list[dict] | Exception | discord.Interaction] | None:
+        """Returns raw data (discord response)"""
         await self.wait()
         return self.data
     
-    async def clean_data(self) -> dict[str, str | list[dict] | Exception] | None:
+    async def clean_data(self) -> dict[str, str | dict[str, str] | Exception] | None:
         raw: dict[str, str | list[dict]] = await self.get_data()
         if not raw or raw.get("error"):
             return raw
