6b0dff7bd03f6d276b14a176ab0ea546695ec87c
xRedCrystalx <xredcrystaledx@gmail.com>
2024-07-06 19:10:23 +0200

    Converted to async, more options, docstring

--- src/core/helpers/errors.py
+++ src/core/helpers/errors.py
@@ -1,22 +1,29 @@
 import sys, typing
 sys.dont_write_bytecode = True
 import src.connector as con
-from .event import Event
 
-from xRedUtils.strings import string_split
-from xRedUtils.errors import simple_error, full_traceback
-from xRedUtils.general import generate_uuid
+from xRedUtilsAsync.strings import string_split
+from xRedUtilsAsync.errors import simple_error, full_traceback
+from xRedUtilsAsync.general import generate_uuid
 
-def report_error(error: Exception | ExceptionGroup, _caller: typing.Callable = None, option: typing.Literal["simple", "full"] = "full", discord: int = 1234547496454459432) -> str:
-    error: str = full_traceback() if option == "full" else simple_error(error) if option == "simple" and error else None
-    if not error:
-        return
+async def report_error(caller: typing.Callable | str = None, option: typing.Literal["simple", "full"] = "full", discord: int = 1234547496454459432) -> str:
+    """
+    Generates and formats error.
+
+    ### Returns:
+    - `error_id` as a `string`.
+    """
     
-    error_id: str = generate_uuid()
-    con.shared.logger.log(f"{f"@{_caller.__qualname__.removeprefix("src.core.")}" if _caller else "An error has occured. "}[{error_id}] > {error}", "ERROR")
+    # check if this actually can work
+    error: str = ("\n" + await full_traceback()) if option == "full" else await simple_error() if option == "simple" else None
+    error_id: str = await generate_uuid()
+
+    formatted_error: str = f"@{caller if isinstance(caller, str) else caller.__qualname__}[{error_id}] > {error} "
+
+    await con.shared.logger.log("ERROR", formatted_error)
 
     if con.shared.bot and (channel := con.shared.bot.get_channel(discord)):
-        for sliced in string_split(error, 1994, option="smart"):
-            con.shared.sender.resolver(Event(channel, "send", event_data={"kwargs": {"content": f"```{sliced}```"}}))
+        for sliced in await string_split(formatted_error, 1994, option="smart"):
+            await con.shared.sender.resolver(con.Event(channel, "send", event_data={"kwargs": {"content": f"```{sliced}```"}}))
 
     return error_id
