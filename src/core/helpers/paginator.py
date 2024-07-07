4624147560d08b47398726520c5dd3904477b58d
xRedCrystalx <xredcrystaledx@gmail.com>
2024-07-07 19:28:18 +0200

    Bug fixes, better customization

--- src/core/helpers/paginator.py
+++ src/core/helpers/paginator.py
@@ -1,14 +1,15 @@
 import sys, discord
 sys.dont_write_bytecode = True
+
 from .views import ViewHelper
-from xRedUtils.dates import get_datetime
+from .errors import report_error
 
 class BasicPaginator(ViewHelper):
     def __init__(self, messages: list[discord.Embed | str], timeout: float | None = None) -> None:
         super().__init__(timeout)
 
-        self.msgList: list = messages
-        self.lenList: int = len(self.msgList)-1
+        self.messages: list[discord.Embed | str] = messages
+        self.lenList: int = len(self.messages)-1
         self.currentPage: int = -1
 
         if messages:
@@ -20,25 +21,28 @@ class BasicPaginator(ViewHelper):
         self.add_item(discord.ui.Button(label="✕", custom_id=f"PAGINATOR:STOP", style=discord.ButtonStyle.red))  
  
     async def paginator(self, value: int | bool, interaction: discord.Interaction) -> None:
-        self.lenList: int = len(self.msgList)-1
-        index: int = 0 if self.currentPage + value > self.lenList else self.lenList if self.currentPage + value < 0 else self.currentPage + value
-        self.currentPage = index
-        if value:
-            if isinstance(self.msgList[index], str):
-                await interaction.response.edit_message(content=f"{self.msgList[index]}\n\nPage {self.currentPage+1}/{self.lenList+1}")
-            
-            elif isinstance(self.msgList[index], discord.Embed):
-                embed: discord.Embed = self.msgList[index]
-                embed.set_footer(text=f"Page {self.currentPage+1}/{self.lenList+1}")
-                embed.timestamp = get_datetime()
-                await interaction.response.edit_message(embed=embed)
+        if value != "STOP":
+            self.currentPage: int = 0 if self.currentPage + value > self.lenList else self.lenList if self.currentPage + value < 0 else self.currentPage + value
+            message: str | discord.Embed = self.messages[self.currentPage]
+
+            if isinstance(message, str):
+                await interaction.response.edit_message(content=f"{message}\n\nPage {self.currentPage+1}/{self.lenList+1}")
+
+            elif isinstance(message, discord.Embed):
+                if not message.footer:
+                    message.set_footer(text=f"Page {self.currentPage+1}/{self.lenList+1}")
+
+                await interaction.response.edit_message(embed=message)
             else:
-                self.shared.logger(f"@BasicPaginator.paginator > An error has occured. Could not find `str` or `discord.Embed` object in list. Index: {index}")
+                await report_error(f"BasicPaginator.paginator<{interaction.guild.id}>", f"An error has occured. Could not find `str` or `discord.Embed` object in list. Index: {self.currentPage}")
         else:
             await interaction.response.edit_message(view=None)
+            self.stop()
 
     async def interaction_check(self, interaction: discord.Interaction) -> bool:
         custom_id: str = dict(interaction.data).get('custom_id')
-        if interaction.type == discord.InteractionType.component and custom_id.startswith("PAGINATOR:"):
-            await self.paginator(value=1 if custom_id.endswith(":NEXT") else -1 if custom_id.endswith(":BACK") else False, interaction=interaction)
-        return True
+        if custom_id.startswith("PAGINATOR:"):
+            await self.paginator(value=1 if custom_id.endswith(":NEXT") else -1 if custom_id.endswith(":BACK") else "STOP", interaction=interaction)
+        else:
+            # Fail or attempted ID manipulation
+            self.stop()
