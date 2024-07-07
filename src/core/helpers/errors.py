2f1c95f2687087c02ba16c18e85272a14780b754
xRedCrystalx <xredcrystaledx@gmail.com>
2024-07-07 19:23:03 +0200

    Added error embed generator and custom error compatibility

--- src/core/helpers/errors.py
+++ src/core/helpers/errors.py
@@ -1,6 +1,8 @@
-import sys, typing
+import sys, typing, discord
 sys.dont_write_bytecode = True
 import src.connector as con
+
+from .embeds import create_base_embed
 
 from xRedUtilsAsync.strings import string_split
 from xRedUtilsAsync.errors import simple_error, full_traceback
@@ -15,7 +17,7 @@ async def report_error(caller: typing.Callable | str = None, option: typing.Lite
     """
     
     # check if this actually can work
-    error: str = ("\n" + await full_traceback()) if option == "full" else await simple_error() if option == "simple" else None
+    error: str = ("\n" + await full_traceback()) if option == "full" else await simple_error() if option == "simple" else error
     error_id: str = await generate_uuid()
 
     formatted_error: str = f"@{caller if isinstance(caller, str) else caller.__qualname__}[{error_id}] > {error} "
@@ -27,3 +29,9 @@ async def report_error(caller: typing.Callable | str = None, option: typing.Lite
             await con.shared.sender.resolver(con.Event(channel, "send", event_data={"kwargs": {"content": f"```{sliced}```"}}))
 
     return error_id
+
+async def new_error_embed(error_id: str) -> discord.Embed:
+    return await create_base_embed(
+        title="Error",
+        description=f"An error has occured. Please report this to the developer.\n**Error code:** `{error_id}`"
+    )
