7f139fc2043181ba8a31eb278366ab1da89d0091
xRedCrystalx <xredcrystaledx@gmail.com>
2024-07-08 18:14:35 +0200

    Better error handler, custom_id for view, other improvements

--- src/core/helpers/views.py
+++ src/core/helpers/views.py
@@ -1,29 +1,23 @@
 import sys, discord
 sys.dont_write_bytecode = True
-import src.connector as con
 from discord.ui import View
 
-from .errors import report_error
+from .errors import report_error, new_error_embed
 
 class ViewHelper(View):
-    def __init__(self, timeout: float | None = 60) -> None:
+    def __init__(self, custom_id: str, timeout: float | None = 60) -> None:
         super().__init__(timeout=timeout)
-        
-        self.shared: con.Shared = con.shared
         self.interaction: discord.Interaction = None
-
-    async def interaction_check(self, interaction: discord.Interaction) -> bool:
-        self.interaction = interaction
-        return await super().interaction_check(interaction)
+        self.custom_id: str = custom_id
     
     async def on_timeout(self) -> None:
         self.stop()
         try:
-            original_message: discord.InteractionMessage = await self.interaction.original_response()
-            await original_message.edit(view=None)
-        # message doesn't exist/can't fetch
+            await self.interaction.edit_original_response(view=None)
+        # original response does not exist
         except: pass
     
     async def on_error(self, interaction: discord.Interaction, error: Exception, item) -> None:
-        error_id: str = report_error(error, self.on_error, "simple")
-        await interaction.response.send_message(f"**An error has occured. Report this to the developer.**\n**Error ID:** `{error_id}`")
+        error_id: str = await report_error(f"ViewHelper.on_error[{self.custom_id}]<{interaction.guild.id}>", "full")
+        embed: discord.Embed = await new_error_embed(error_id)
+        await interaction.response.send_message(embed=embed, ephemeral=True)
