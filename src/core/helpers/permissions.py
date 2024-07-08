c95126a163f1d96c1fed43bce8276182d5a56d80
xRedCrystalx <xredcrystaledx@gmail.com>
2024-07-08 18:01:23 +0200

    Async compatibility, better names and algorithms

--- src/core/helpers/permissions.py
+++ src/core/helpers/permissions.py
@@ -1,26 +1,31 @@
 import sys, typing
 sys.dont_write_bytecode = True
-from discord import Permissions, ChannelType
+from discord import Permissions, ChannelType, Member
+from discord.abc import GuildChannel, PrivateChannel
 from src.connector import shared
 
-from xRedUtils.iterables import compare_iterables, get_attr_data
+from xRedUtilsAsync.iterables import compare_iterables, get_attr_data, to_iterable
 
+async def check_ids(ids1: list[int | object], ids2: list[int | object]) -> list[int]:
+    return await compare_iterables(
+        await get_attr_data(await to_iterable(ids1), "id"),
+        await get_attr_data(await to_iterable(ids2), "id")
+    )
 
-def check_ids(ids1: list[int | object], ids2: list[int | object]) -> list[int]:
-    if not isinstance(ids1, list):
-        ids1 = [ids1]
+# More brainstorming required
+async def check_permissions(current_perms: list[Permissions], required_perms: ...) -> list[Permissions]:
+    raise NotImplementedError()
 
-    return compare_iterables(get_attr_data(ids1, "id"), get_attr_data(ids2, "id"))
+    return await compare_iterables(
+        [getattr(Permissions, perm, None) for perm in required_perms],
+        current_perms)
 
-def check_id(current: object | int, required: object | int) -> bool:
-    return getattr(current, "id", current) == getattr(required, "id", required)
-
-def check_permissions(current_perms: list[Permissions], required_perms: ...) -> list[Permissions]:
-    required_perms = [getattr(Permissions, perm, None) for perm in required_perms]
-    return compare_iterables(required_perms, current_perms)
-
-def check_channel_type(current_location: ChannelType, required_location: typing.Literal["text", "category", "forum", "group", "news", "news_thread", "private", "private_thread", "public_thread", "voice", "stage_voice"]) -> bool:
+async def check_channel_type(current_location: GuildChannel | PrivateChannel | ChannelType, required_location: typing.Literal["text", "category", "forum", "group", "news", "news_thread", "private", "private_thread", "public_thread", "voice", "stage_voice"]) -> bool:
+    current_location = current_location if isinstance(current_location, ChannelType) else getattr(current_location, "type", None)
     return current_location == getattr(ChannelType, required_location, None)
 
-def check_bot_owner(member_id: int)-> bool:
-    return member_id in shared.db.load_data().get("owners")
+async def isGuildAdministrator(member: Member) -> bool:
+    return member.guild_permissions.administrator or (member.id == member.guild.owner.id)
+
+async def isBotAdministrator(member: Member | int) -> bool:
+    return (member if isinstance(member, int) else member.id) in shared.db.load_data().get("owners")
