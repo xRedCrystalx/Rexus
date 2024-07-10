5d547e6e0c79ca9cf80480d94e3b3f1028a00f56
xRedCrystalx <xredcrystaledx@gmail.com>
2024-07-10 16:17:42 +0200

    Converted back to sync due to no performance change

--- src/core/helpers/embeds.py
+++ src/core/helpers/embeds.py
@@ -2,11 +2,13 @@ import sys, discord
 sys.dont_write_bytecode = True
 from discord import Embed
 
-from xRedUtilsAsync.dates import get_datetime
-from xRedUtilsAsync.type_hints import SIMPLE_ANY
+from xRedUtils.dates import get_datetime
+from xRedUtils.type_hints import SIMPLE_ANY
 
-async def new_embed(title: str = "", thumbnail: str = "", footer: str | dict = None, **kwargs) -> Embed:
+def new_embed(title: str = "", thumbnail: str = None, footer: str | dict = None, **kwargs) -> Embed:
     """
+    |sync|
+
     Simple mostly used function. Use `create_base_embed` and `apply_embed_items` for more customization.
 
     Base embed:
@@ -22,22 +24,25 @@ async def new_embed(title: str = "", thumbnail: str = "", footer: str | dict = N
     - `thumbnail` - `str` > URL of thumbnail (on the side image)
     - `footer` - `dict[text: str = None, icon_url: str = None]` | `str` > Everything for footer
     """
-    return await apply_embed_items(
-        embed= await create_base_embed(title=title, **kwargs),
+    return apply_embed_items(
+        embed= create_base_embed(title=title, **kwargs),
         author=kwargs.get("author"),
         thumbnail=thumbnail,
         image=kwargs.get("image"),
         footer=footer
     )
 
-async def create_base_embed(title: str = "", description: str = None, **kwargs) -> Embed:
+def create_base_embed(title: str = "", description: str = None, **kwargs) -> Embed:
+    """|sync|"""
     return discord.Embed(title=title, description=description, 
                         color=kwargs.get("color") or discord.Colour.dark_embed(),
                         url=kwargs.get("url"),
-                        timestamp=kwargs.get("timestamp") or await get_datetime())
+                        timestamp=kwargs.get("timestamp") or get_datetime())
 
-async def apply_embed_items(embed: discord.Embed, author: dict[str, str] = None, thumbnail: str = None, image: str = None, footer: str | dict[str, str] = None) -> Embed:
+def apply_embed_items(embed: discord.Embed, author: dict[str, str] = None, thumbnail: str = None, image: str = None, footer: str | dict[str, str] = None) -> Embed:
     """
+    |sync|
+
     Adds data to the embed.
     - author: `dict[name: str, url: str = None, icon_url: str = None]` > Author field (Small thing on top, `url` parameter is for link)
     - image: `str` > URL of image (under the text image)
@@ -57,8 +62,12 @@ async def apply_embed_items(embed: discord.Embed, author: dict[str, str] = None,
 
     return embed
 
-async def to_embed(embed: dict[str, SIMPLE_ANY]) -> Embed:
+def to_embed(embed: dict[str, SIMPLE_ANY]) -> Embed:
+    """|sync|"""
     return discord.Embed.from_dict(embed)
 
-async def to_dict(embed: Embed) -> dict[str, SIMPLE_ANY]:
+def to_dict(embed: Embed) -> dict[str, SIMPLE_ANY]:
+    """|sync|"""
     return discord.Embed.to_dict(embed)
+
+NO_PERMISSIONS: Embed = new_embed(title="No permissions", description="You are not allowed to do that!", color=discord.Colour.red())
