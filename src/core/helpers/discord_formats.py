4e52f367d35314e132372af92eda15002417c6f2
xRedCrystalx <xredcrystaledx@gmail.com>
2024-07-11 22:48:09 +0200

    Added more formats, imported some important functions from discord.utils

--- src/core/helpers/discord_formats.py
+++ src/core/helpers/discord_formats.py
@@ -1,25 +1,49 @@
 import sys
 sys.dont_write_bytecode = True
+from discord.utils import (format_dt, 
+                           escape_markdown, 
+                           remove_markdown, 
+                           parse_time, 
+                           escape_mentions)
 
-def code_block(string: str, lang: str = "") -> str:
-    return f"```{lang}\n{string}```"
+def italics(s: str) -> str:
+    return f"*{s}*"
 
-def one_line_code(string: str) -> str:
-    return f"`{string}`"
+def bold(s: str) -> str:
+    return f"**{s}**"
 
-def bold(string: str) -> str:
-    return f"**{string}**"
+def bolitalic(s: str) -> str:
+    """Bold and italic"""
+    return f"***{s}***"
 
-def italics(string: str) -> str:
-    return f"*{string}*"
+def underline(s: str) -> str:
+    return f"__{s}__"
 
-def underline(string: str) -> str:
-    return f"__{string}__"
+def strike_through(s: str) -> str:
+    return f"~~{s}~~"
 
-def strike_through(string: str) -> str:
-    return f"~~{string}~~"
+def one_line_code(s: str) -> str:
+    return f"`{s}`"
 
-def masked(name: str, link: str) -> str:
+def code_block(s: str, lang: str = "") -> str:
+    return f"```{lang}\n{s}```"
+
+def masked_link(name: str, link: str) -> str:
     return f"[{name}]({link})"
 
-# ignoring headers, quotes and lists due to levels
+def header(s: str, level: int) -> str:
+    return "#"*level + s
+
+def quote(s: str) -> str:
+    return f"> {s}"
+
+def spoiler(s: str) -> str:
+    return f"||{s}||"
+
+def subtext(s: str) -> str:
+    return f"-#{s}"
+
+def listing(s: list[str]) -> str:
+    return "\n".join([f"- {part}" for part in s])
+
+# TODO: organizational text formatting - experimental
