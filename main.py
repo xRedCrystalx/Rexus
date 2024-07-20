afe61021078e27c01b0ca51bd9e2a53f21751c2e
xRedCrystalx <xredcrystaledx@gmail.com>
2024-07-20 18:49:23 +0200

    New colors, general fixes

--- main.py
+++ main.py
@@ -1,69 +1,75 @@
-import discord, aiohttp, os, sys, glob
+import discord, aiohttp, os, sys, glob, logging, pprint
 sys.dont_write_bytecode = True
 from discord.ext import commands
-import src.connector as con
+from src.connector import shared
 
+from xRedUtilsAsync.errors import full_traceback
+from xRedUtilsAsync.colors import (
+    Foreground16 as FG,
+    Style as ST
+)
 
 class MyBot(commands.AutoShardedBot):
     def __init__(self) -> None:
-        super().__init__(command_prefix="-", intents=discord.Intents.all(), shard_count=2, help_command=None)
-        self.shared: con.Shared = con.shared
+        super().__init__(command_prefix="-", intents=discord.Intents.all(), shard_count=2, help_command=None, max_messages=100)
 
-        self.shared.path = os.path.dirname(os.path.realpath(__file__))
-        self.c = self.shared.colors
-        self.shared.bot = self
+        shared.path = os.path.dirname(os.path.realpath(__file__))
+        shared.bot = self
 
     async def setup_hook(self) -> None:
-        self.shared.system_load()
-        self.shared.plugin_load()
-        self.session = self.shared.session = aiohttp.ClientSession()
+        shared.system_load()
+        self.session = shared.session = aiohttp.ClientSession()
 
-        print(rf"""{self.c.Yellow}
-    _   __      ____  _            
+        print(rf"""{FG.YELLOW}
+    _   __      ____  _
    / | / /___  / __ \(_)___  ____ _
   /  |/ / __ \/ /_/ / / __ \/ __ `/
  / /|  / /_/ / ____/ / / / / /_/ / 
 /_/ |_/\____/_/   /_/_/ /_/\__, /  
                           /____/   
-{self.c.R}Loading... Please wait. 
-{self.c.Gray}──────────────────────────────────────────────────{self.c.R}""")
-        print(f"Found {self.c.Magenta}{len(os.listdir(f"{self.shared.path}/databases/guilds"))}{self.c.R} server databases.")
-        print(f"Found {self.c.Magenta}{len(os.listdir(f"{self.shared.path}/databases/users"))}{self.c.R} user databases.")
-        print(f"{self.c.Gray}──────────────────────────────────────────────────{self.c.R}")
+{ST.RESET}Loading... Please wait. 
+{FG.BLACK}──────────────────────────────────────────────────{ST.RESET}""")
+        print(f"Found {FG.MAGENTA}{len(os.listdir(f"{shared.path}/databases/guilds"))}{ST.RESET} server databases.")
+        print(f"Found {FG.MAGENTA}{len(os.listdir(f"{shared.path}/databases/users"))}{ST.RESET} user databases.")
+        print(f"{FG.BLACK}──────────────────────────────────────────────────{ST.RESET}")
 
-        cogPaths: tuple[str, ...] = ("src/core/listeners", "src/core/commands")
+        cogPaths: tuple[str, ...] = ("src/core/listeners", "src/core/commands", "src/core/plugins")
         for path in cogPaths:
-            print(f"{self.c.Green}+{self.c.R} Loading {path}..")
+            print(f"{FG.GREEN}+{ST.RESET} Loading {path}..")
             counter = 0
             for cog in (cogList := [*glob.glob(f"{path}/*.py"), *glob.glob(f"{path}/*/*.py")]):
                 try:
                     await self.load_extension(cog.replace("\\", ".").replace("/", ".").removesuffix(".py"))
                     counter += 1
                 except Exception:
-                    print(f"{self.c.Red}Failed to load {cog}:\n{self.shared.errors.full_traceback(False)}{self.c.R}")
-            
-            print(f"{self.c.Cyan}i{self.c.R} Successfully loaded {self.c.Cyan}{counter}/{len(cogList)}{self.c.R} extensions.")
+                    print(f"{FG.RED}Failed to load {cog}:\n{await full_traceback()}{ST.RESET}")
+
+            print(f"{FG.CYAN}i{ST.RESET} Successfully loaded {FG.CYAN}{counter}/{len(cogList)}{ST.RESET} extensions.")
         
-        print(f"{self.c.Gray}──────────────────────────────────────────────────{self.c.R}")
+        print(f"{FG.BLACK}──────────────────────────────────────────────────{ST.RESET}")
         if not (synced := await self.tree.sync()):
-            print(f"{self.c.Red}Syncing failed.{self.c.R}")
+            print(f"{FG.RED}Syncing failed.{ST.RESET}")
         else:
-            print(f"{self.c.Green}Synced {len(synced)} commands globally{self.c.R}")
+            print(f"{FG.GREEN}Synced {len(synced)} commands globally{ST.RESET}")
 
         if not (testing_guild_sync := await self.tree.sync(guild=discord.Object(id=1230040815116484678))):
-            print(f"{self.c.Red}Syncing to testing guild failed.{self.c.R}")
+            print(f"{FG.RED}Syncing to testing guild failed.{ST.RESET}")
         else:
-            print(f"{self.c.Green}Synced {len(testing_guild_sync)} commands to testing guild.{self.c.R}")
+            print(f"{FG.GREEN}Synced {len(testing_guild_sync)} commands to testing guild.{ST.RESET}")
 
-        print(f"{self.c.Gray}──────────────────────────────────────────────────{self.c.R}\nConsole:")
+        print(f"{FG.BLACK}──────────────────────────────────────────────────{ST.RESET}\nConsole:")
 
     async def close(self) -> None:
         await super().close()
         await self.session.close()
 
+    async def on_command_error(self, ctx: commands.Context, error: commands.CommandError) -> None:
+        if not isinstance(error, commands.CommandNotFound):
+            await super().on_command_error(ctx, error)
+
     async def on_ready(self) -> None:
-        print(f"{self.c.Magenta}{self.user} has connected to Discord!{self.c.R}")
-        await self.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.playing, name="V3 out!"))
+        print(f"{FG.MAGENTA}{self.user} has connected to Discord!{ST.RESET}")
+        await self.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.playing, name="Hi there :)"))
 
 if __name__ == "__main__":
-    MyBot().run("SECRET-TOKEN", reconnect=True, log_handler=None)
+    MyBot().run("TOKEN", reconnect=True, log_level=logging.INFO)
