df375f6fcf85650f36518138364f26952b31f0ee
xRedCrystalx <xredcrystaledx@gmail.com>
2024-07-28 01:31:45 +0200

    Refresh

--- src/core/commands/staffCMD/slowmode_cmd.py
+++ src/core/commands/staffCMD/slowmode_cmd.py
@@ -1,23 +1,22 @@
 import discord, sys, typing
 sys.dont_write_bytecode = True
 from discord.ext import commands
-import src.connector as con
+from src.connector import shared
 
-from src.core.helpers.permissions import check_ids, check_bot_owner
+from src.core.helpers.permissions import check_ids
 from src.core.helpers.errors import report_error
 
-from xRedUtils.times import seconds_to_str
+from xRedUtilsAsync.times import seconds_to_str
 
 class SlowmodeCommand(commands.Cog):
     def __init__(self, bot: commands.AutoShardedBot) -> None:
-        self.shared: con.Shared = con.shared
         self.bot: commands.AutoShardedBot = bot
 
     @discord.app_commands.command(name="slowmode", description="Set slowmode. (in seconds, 0 = No slowmode)")
     async def slowmode(self, interaction: discord.Interaction, set: int, channel: discord.TextChannel | None = None) -> None:
-        guild_db: dict[str, typing.Any] = self.shared.db.load_data(interaction.guild.id)
+        guild_db: dict[str, typing.Any] = shared.db.load_data(interaction.guild.id)
 
-        if check_ids(interaction.user.roles, [guild_db["general"]["staffRole"], guild_db["general"]["adminRole"]]) or check_bot_owner(interaction.user.id):
+        if check_ids(interaction.user.roles, [guild_db["general"]["staffRole"], guild_db["general"]["adminRole"]]):# or check_bot_owner(interaction.user.id)
             if not channel:
                 channel = interaction.channel
 
@@ -27,10 +26,10 @@ class SlowmodeCommand(commands.Cog):
 
             try:
                 await channel.edit(slowmode_delay=set)
-                await interaction.response.send_message(f"Successfully set **{channel.name}'s** slowmode to `{seconds_to_str(set)}`.", ephemeral=True)
+                await interaction.response.send_message(f"Successfully set **{channel.name}'s** slowmode to `{await seconds_to_str(set)}`.", ephemeral=True)
                     
             except Exception as error:
-                error_id: str = report_error(error, self.slowmode, "full")
+                error_id: str = await report_error(self.slowmode, "full")
                 await interaction.response.send_message(f"Failed to set slowmode. Error ID: `{error_id}`", ephemeral=True)
         else:
             await interaction.response.send_message("You do not have permissions to execute this command.", ephemeral=True)
--- src/core/commands/staffCMD/user_bulk_delete.py
+++ src/core/commands/staffCMD/user_bulk_delete.py
@@ -1,24 +1,23 @@
 import discord, sys, typing
 sys.dont_write_bytecode = True
 from discord.ext import commands
-import src.connector as con
+from src.connector import shared
 
-from src.core.helpers.permissions import check_ids, check_bot_owner
+from src.core.helpers.permissions import check_ids
 from src.core.helpers.errors import report_error
 
 class BulkDelete(commands.Cog):
     def __init__(self, bot: commands.AutoShardedBot) -> None:
-        self.shared: con.Shared = con.shared
         self.bot: commands.AutoShardedBot = bot
 
     @discord.app_commands.command(name="purge", description="Purges user's messages.")
     async def purge(self, interaction: discord.Interaction, member: discord.Member, number: int | None = None) -> None:
-        guild_db: dict[str, typing.Any] = self.shared.db.load_data(interaction.guild.id)
+        guild_db: dict[str, typing.Any] = shared.db.load_data(interaction.guild.id)
 
         def target(m: discord.Message) -> bool:
             return m.author.id == member.id
 
-        if check_ids(interaction.user.roles, guild_db["general"]["staffRole"]) or check_bot_owner(interaction.user.id):
+        if check_ids(interaction.user.roles, guild_db["general"]["staffRole"]):# or check_bot_owner(interaction.user.id)
             try:
                 await interaction.response.send_message(f"Purging {member.display_name}'s messages. Checking last {number if number else "100"} messages per channel.", ephemeral=True)
                 
