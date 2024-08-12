36957242e0c44d8578887555383b3f9482d861a4
xRedCrystalx <xredcrystaledx@gmail.com>
2024-08-12 20:28:05 +0200

    patch with import

--- src/core/listeners/voice.py
+++ src/core/listeners/voice.py
@@ -1,16 +1,15 @@
 import discord, sys
 sys.dont_write_bytecode = True
 from discord.ext import commands
-import src.connector as con
+from src.connector import shared
 
-class VoiceListeners(commands.Cog):
+class VoiceListeners(commands.AutoShardedBot):
     def __init__(self, bot: commands.Bot) -> None:
-        self.shared: con.Shared = con.shared
         self.bot: commands.Bot = bot
 
     @commands.Cog.listener()
     async def on_voice_state_update(self, member: discord.Member, before: discord.VoiceState, after: discord.VoiceState) -> None:
-        self.shared.loop.create_task(self.shared.queue.add_to_queue(e="on_voice_state_update", guild_id=member.guild.id, member=member, before=before, after=after))
+        shared.loop.create_task(shared.queue.add_to_queue(e="on_voice_state_update", guild_id=member.guild.id, member=member, before=before, after=after))
 
-async def setup(bot: commands.Bot) -> None:
+async def setup(bot: commands.AutoShardedBot) -> None:
     await bot.add_cog(VoiceListeners(bot))
