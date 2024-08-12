75165a08c9bc739f5fc9c149f068cdbc9ccb7f22
xRedCrystalx <xredcrystaledx@gmail.com>
2024-08-12 20:11:26 +0200

    changed table names

--- database/triggers/new_guild.sql
+++ database/triggers/new_guild.sql
@@ -8,14 +8,14 @@ CREATE TRIGGER guild_setup
 AFTER INSERT ON plugins
     FOR EACH ROW BEGIN
         -- Starts inserting data
-        INSERT INTO general (guild_id) VALUES (NEW.guild_id);
-        INSERT INTO cmd (guild_id) VALUES (NEW.guild_id);
-        INSERT INTO alt (guild_id) VALUES (NEW.guild_id);
-        INSERT INTO imper (guild_id) VALUES (NEW.guild_id);
-        INSERT INTO automod (guild_id, rules) VALUES (NEW.guild_id, JSON_ARRAY());
-        INSERT INTO link (guild_id, options) VALUES (NEW.guild_id, JSON_ARRAY());
-        INSERT INTO ping (guild_id, ignoredChannels, rules) VALUES (NEW.guild_id, JSON_ARRAY(), JSON_ARRAY());
-        INSERT INTO auto_delete (guild_id, monitored) VALUES (NEW.guild_id, JSON_ARRAY());
-        INSERT INTO auto_slowmode (guild_id, monitored) VALUES (NEW.guild_id, JSON_ARRAY());
-        INSERT INTO reaction (guild_id) VALUES (NEW.guild_id);
+        INSERT INTO pl_general (guild_id) VALUES (NEW.guild_id);
+        INSERT INTO pl_cmd (guild_id) VALUES (NEW.guild_id);
+        INSERT INTO pl_alt (guild_id) VALUES (NEW.guild_id);
+        INSERT INTO pl_imper (guild_id) VALUES (NEW.guild_id);
+        INSERT INTO pl_automod (guild_id, rules) VALUES (NEW.guild_id, JSON_ARRAY());
+        INSERT INTO pl_link (guild_id, options) VALUES (NEW.guild_id, JSON_ARRAY());
+        INSERT INTO pl_ping (guild_id, ignoredChannels, rules) VALUES (NEW.guild_id, JSON_ARRAY(), JSON_ARRAY());
+        INSERT INTO pl_auto_delete (guild_id, monitored) VALUES (NEW.guild_id, JSON_ARRAY());
+        INSERT INTO pl_auto_slowmode (guild_id, monitored) VALUES (NEW.guild_id, JSON_ARRAY());
+        INSERT INTO pl_reaction (guild_id) VALUES (NEW.guild_id);
 END;
